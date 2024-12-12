ae6adaf140f4abab8247452bff926c7560431280
fork_IVK <ivkor72@mail.ru>
2024-12-12 03:37:37 +0300

    2 стадия

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,10 +1,15 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import com.mchange.v2.util.DoubleWeakHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +19,7 @@ import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.persistence.EntityManager;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class AdminController {
@@ -30,16 +34,57 @@ public class AdminController {
         this.passwordEncoder = passwordEncoder;
         this.roleDao = roleDao;
     }
+    public static class UsersAndRoles {
+        private String username;
+        private boolean enabled;
+        private List<Role> roles;
+        UsersAndRoles(String username, boolean enabled, List<Role> roles) {
+            this.username = username;
+            this.enabled = enabled;
+            this.roles = roles;
+        }
 
-    @RequestMapping(value = "/admin")
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public List<Role> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<Role> roles) {
+            this.roles = roles;
+        }
+    }
+    @GetMapping(value = "/admin")
     public String showAllUsers(ModelMap model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentusername = authentication.getName();
+        User currentuser = userService.getUser(currentusername);
+        List<Role> currentuserroles = roleDao.findRolesByUser(currentusername);
+        model.addAttribute("currentusername", currentusername);
+        model.addAttribute("currentuserroles", currentuserroles);
         List<String> messages = new ArrayList<>();
-        messages.add("Hello, admin!");
-        messages.add("All Users:");
-        model.addAttribute("messages", messages);
         List<User> allUsers = userService.getAllUsers();
-        System.out.println("All Users: " + allUsers);
-        model.addAttribute("users", allUsers);
+        List<Role> roles = new ArrayList<>();
+        List<UsersAndRoles> usersAndRoles = new ArrayList<>();
+        for (User user : allUsers) {
+            roles = roleDao.findRolesByUser(user.getUsername());
+            usersAndRoles.add(new UsersAndRoles(user.getUsername(), user.getEnabled(), roles));
+        }
+        model.addAttribute("usersAndRoles", usersAndRoles);
         return "show";
     }
 
