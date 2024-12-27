ce6ecffdd30d3ec670e49d266293574bfb2547bd
fork_IVK <ivkor72@mail.ru>
2024-12-28 02:20:41 +0300

    Окончательная версия

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -16,7 +16,6 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    @Id
 
     @PrimaryKeyJoinColumn(name = "users.username")
     @Column(name = "username")
