b95d65bb3d9bf73c5baeff51e105a7d9edd9911e
fork_IVK <ivkor72@mail.ru>
2025-03-12 02:39:12 +0300

    окончательная версия (с исправлениями) 2

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.model.User;
 import java.util.List;
 
-public interface UserDao extends JpaRepository<User, Long> {
+public interface UserDao {
 
     public List<User> getAllUsers();
 
--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
@@ -1,11 +1,6 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,9 +11,6 @@ import ru.kata.spring.boot_security.demo.model.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 public class UserDaoImpl implements UserDao {
@@ -31,11 +23,6 @@ public class UserDaoImpl implements UserDao {
     public UserDaoImpl(BCryptPasswordEncoder passwordEncoder, RoleDao roleDao) {
         this.passwordEncoder = passwordEncoder;
         this.roleDao = roleDao;
-    }
-
-    @PersistenceContext
-    public void setEntityManager(EntityManager entityManager) {
-        this.em = entityManager;
     }
 
     @Override
@@ -87,11 +74,6 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    public List<User> findAll() {
-        return List.of();
-    }
-
-    @Override
     public User findUByUsername(String username) {
         return null;
     }
@@ -102,141 +84,5 @@ public class UserDaoImpl implements UserDao {
         String role = roleDao.findRoleByIdy(user.getId());
         System.out.println("UPDATE USER role= " + role + "user = " + user);
         saveUser(user, role);
-    }
-
-    @Override
-    public List<User> findAll(Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public Page<User> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public List<User> findAllById(Iterable<Long> longs) {
-        return List.of();
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-    }
-
-    @Override
-    public void delete(User entity) {
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> longs) {
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends User> entities) {
-    }
-
-    @Override
-    public void deleteAll() {
-    }
-
-    @Override
-    public <S extends User> S save(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> List<S> saveAll(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public Optional<User> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public void flush() {
-    }
-
-    @Override
-    public <S extends User> S saveAndFlush(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> List<S> saveAllAndFlush(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public void deleteAllInBatch(Iterable<User> entities) {
-    }
-
-    @Override
-    public void deleteAllByIdInBatch(Iterable<Long> longs) {
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-    }
-
-    @Override
-    public User getOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public User getById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public User getReferenceById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> Optional<S> findOne(Example<S> example) {
-        return Optional.empty();
-    }
-
-    @Override
-    public <S extends User> List<S> findAll(Example<S> example) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> long count(Example<S> example) {
-        return 0;
-    }
-
-    @Override
-    public <S extends User> boolean exists(Example<S> example) {
-        return false;
-    }
-
-    @Override
-    public <S extends User, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-        return null;
     }
 }
